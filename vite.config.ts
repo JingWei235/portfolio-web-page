98c63b7f3e81d0ee6d32cd93aafcd7c3f3befd38
Ong Jing Wei <jwong@fluxia.com>
2026-01-08 00:27:10 +0800

    changed from javascript to typescript

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-web-page/",
+  build: {
+    outDir: "dist"
+  }
 })
